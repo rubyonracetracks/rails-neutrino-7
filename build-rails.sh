8b292389b4b50ca8896b2ed80180aa50cf865972
Jason Hsu <jhsu802701@jasonhsu.com>
2021-10-30 20:59:13 +0000

    Added 01-03

--- build-rails.sh
+++ build-rails.sh
@@ -158,7 +158,7 @@ fi
 if [ "$UNIT_01" = 'Y' ]
 then
   cd $DIR_APP && bash mod_app.sh '01-02' $TOGGLE_OUTLINE
-  # cd $DIR_APP && bash mod_app.sh '01-03' $TOGGLE_OUTLINE
+  cd $DIR_APP && bash mod_app.sh '01-03' $TOGGLE_OUTLINE
   # cd $DIR_APP && bash mod_app.sh '01-04' $TOGGLE_OUTLINE
   # cd $DIR_APP && bash mod_app.sh '01-05' $TOGGLE_OUTLINE
   # cd $DIR_APP && bash mod_app.sh '01-06' $TOGGLE_OUTLINE
