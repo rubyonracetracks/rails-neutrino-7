f1244eb02201cea4936bffbc12acc40ed565e929
Jason Hsu <jhsu802701@jasonhsu.com>
2021-11-08 14:25:13 -0600

    docker/sandi_meter works

---
+++ mod/mod-02-05/mod-02-05.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/ruby
+# frozen_string_literal: true
+
+require 'insert_from_file'
+require 'line_containing'
+require 'gemfile_entry'
+require 'string_in_file'
+
+puts 'Updating the Gemfile'
+LineContaining.add_after("gem 'rubocop-rails'", "  # gem 'sandi_meter'", 'Gemfile')
