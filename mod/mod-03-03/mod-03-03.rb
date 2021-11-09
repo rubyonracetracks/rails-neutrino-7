6c52becc26333450fdd527b2067340beb0cbea98
Jason Hsu <jhsu802701@jasonhsu.com>
2021-11-09 18:28:42 +0000

    03-03 (bundler outdated) works

---
+++ mod/mod-03-03/mod-03-03.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/ruby
+# frozen_string_literal: true
+
+require 'insert_from_file'
+require 'string_in_file'
+require 'gemfile_entry'
+require 'replace_quotes'
+require 'remove_double_blank'
+require 'line_containing'
+
+puts 'Enabling docker/bundle_outdated in docker/test_code'
+StringInFile.replace('# docker/bundle_outdated', 'docker/bundle_outdated', 'docker/test_code')
+system('chmod +x docker/test_code')
