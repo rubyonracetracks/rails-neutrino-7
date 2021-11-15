acd808b2954ef0120cd49463f02d77ebe57dbfd6
Jason Hsu <jhsu802701@jasonhsu.com>
2021-11-15 10:57:02 -0600

    Removed Git branching

--- mod/mod-04-01/mod-04-01.sh
+++ mod/mod-04-01/mod-04-01.sh
@@ -18,11 +18,7 @@ echo '######################################'
 echo 'Unit 4 Chapter 1: Updating the Gemfile'
 echo '######################################'
 
-git checkout -b 04-01-update_gemfile
-
 ruby mod-04-01.rb
 
 git add .
 git commit -m "Updated the Gemfile (pg for production environment)"
-git checkout master
-git merge 02-01-update_gemfile
