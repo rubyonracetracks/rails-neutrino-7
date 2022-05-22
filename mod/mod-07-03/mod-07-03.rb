1c6e88988c89f7bfbd19430d79d24a19f7cc8e73
Jason Hsu <jhsu802701@jasonhsu.com>
2022-05-22 21:50:49 +0000

    rails_erd -> rails-erd

--- mod/mod-07-03/mod-07-03.rb
+++ mod/mod-07-03/mod-07-03.rb
@@ -18,17 +18,17 @@ system('chmod +x bin/replace_sif')
 system('mv mod-07-03-bin-railroady bin/railroady')
 system('chmod +x bin/railroady')
 
-# WICHTIG: Add bin/rails_erd for using the rails-erd gem
-system('mv mod-07-03-bin-rails_erd bin/rails_erd')
-system('chmod +x bin/rails_erd')
+# WICHTIG: Add bin/rails-erd for using the rails-erd gem
+system('mv mod-07-03-bin-rails-erd bin/rails-erd')
+system('chmod +x bin/rails-erd')
 
 # WICHTIG: Add docker/railroady for running bin/railroady in Docker
 system('mv mod-07-03-docker-railroady docker/railroady')
 system('chmod +x docker/railroady')
 
-# WICHTIG: Add docker/rails_erd for running bin/rails_erd in Docker
-system('mv mod-07-03-docker-rails_erd docker/rails_erd')
-system('chmod +x docker/rails_erd')
+# WICHTIG: Add docker/rails-erd for running bin/rails-erd in Docker
+system('mv mod-07-03-docker-rails-erd docker/rails-erd')
+system('chmod +x docker/rails-erd')
 
 # WICHTIG: Add docker/outline-long for executing bin/railroady and bin/rails-erd in Docker
 system('mv mod-07-03-docker-outline-long docker/outline-long')
